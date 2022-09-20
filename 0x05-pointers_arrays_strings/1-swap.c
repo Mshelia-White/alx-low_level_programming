2aa0b9f5aec8314fad751e171d04fb3aa9093432
Mshelia Promise <msheliapromise@gmail.com>
2022-09-20 05:10:30 +0100

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - Write a function that swaps the values of two integers.
+ *
+ * @a: the variable pointer at a
+ * @b: the variable pointer at b
+ */
+
+void swap_int(int *a, int *b)
+{
+int i = *a;
+*a = *b;
+*b = i;
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
